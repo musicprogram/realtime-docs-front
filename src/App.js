0891e36c54313763dcb21c141472dd9e2e6a63ca
musicprogram <jahenao17@misena.edu.co>
2020-08-14 14:00:59 -0500

    connect with rails app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
+const apiUrl = 'http://localhost:3001/notes/1';
+
+
 function App() {
+  const [text,setText] = useState('');
+
+  useEffect(()=>{
+    window.fetch(apiUrl).then(data => {
+      data.json().then(res => {
+        setText(res.text);
+      })
+    })
+  },[]);
+
+  const handleChangeText = (e) =>{
+    e.preventDefault();
+    setText(e.target.value);
+  }
+
   return (
     <div className="App">
-
+      <textarea
+        value={text}
+        onChange={handleChangeText}
+        cols="30"
+        rows="10"/>
     </div>
   );
 }
